cb60c9d8c38f4f616c3055f512e6a0f5fc441ffc
Akshat Gupta <akshatgupta0030@gmail.com>
2024-03-17 12:55:45 +0530

    Refactored Api service

---
+++ lib/src/services/api_urls.dart
@@ -0,0 +1,3 @@
+const String baseUrl = 'https://api.openai.com/v1';
+
+const String chatUrl = '$baseUrl/chat/completions';
--- lib/src/services/chat_completion_api.dart
+++ lib/src/services/chat_completion_api.dart
@@ -1,16 +1,16 @@
 import 'dart:convert';
 
 import 'package:chit_chat_pro/src/model/response.dart';
+import 'package:chit_chat_pro/src/services/api_urls.dart';
 import 'package:chit_chat_pro/utils/enums/gpt_model.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
 
-class OpenAiApi {
+class ChatCompletionApi {
   static Future<Map<String, dynamic>> _apiCall(GPTmodel model, String prompt) async {
     const maxTokens = 50;
     const temperatures = 0.8;
-    const uri = 'https://api.openai.com/v1/chat/completions';
 
     final headers = {
       'Content-Type': 'application/json',
@@ -23,7 +23,7 @@ class OpenAiApi {
       'temperature': temperatures,
     };
 
-    final response = await http.post(Uri.parse(uri), headers: headers, body: requestBody);
+    final response = await http.post(Uri.parse(chatUrl), headers: headers, body: requestBody);
     if(response.statusCode != 200) {
       "Open Ai Api\nStatus Code : ${response.statusCode}\nMessage : ${jsonDecode(response.body)['message']}".printError();
       return {};
@@ -31,7 +31,7 @@ class OpenAiApi {
     return jsonDecode(response.body);
   }
 
-  static Future<ChatResponse> getResponse(GPTmodel model, String prompt) async {
+  static Future<ChatResponse> getChat(GPTmodel model, String prompt) async {
     final result = await _apiCall(model, prompt);
     return ChatResponse.fromJson(result);
   }
