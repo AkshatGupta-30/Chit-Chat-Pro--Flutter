e8567a77c62510c840cc6c2eac7fd8104c5185f5
Akshat Gupta <akshatgupta0030@gmail.com>
2024-04-03 20:27:48 +0530

    Removed Firebase configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
-import 'package:chit_chat_pro/firebase_options.dart';
 import 'package:chit_chat_pro/src/pages/home_page.dart';
 import 'package:chit_chat_pro/utils/constants/themes.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
@@ -9,7 +7,6 @@ import 'package:get/get_navigation/src/root/get_material_app.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MainApp());
 }
 
